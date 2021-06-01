3e3e6e70a43fbdddc3f44a96161acd93f43316f7
Amanda Garrison <amanda.garrison@improving.com>
2021-06-01 08:57:04 -0500

    Add project files.

---
+++ KnockKnock/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KnockKnock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Knock Knock");
+
+            string who = (Console.ReadLine());
+            if (who == "who's there?")
+
+            {
+                Console.WriteLine("Boo");
+            }
+            else if (who == "Who is there?")
+            {
+                Console.WriteLine("Boo");
+            }
+            else if (who == "who's there")
+            {
+                Console.WriteLine("Boo");
+            }
+
+            else
+            {
+                Console.WriteLine("Knock Knock");
+            }
+
+            //Console.ReadLine();
+            if(Console.ReadLine() == "Boo who?")
+            {
+                Console.WriteLine("Why are you crying?");
+            }
+            else if (Console.ReadLine() == "boo who")
+            {
+                Console.WriteLine("Why are you crying?");
+            }
+            else
+            {
+                Console.WriteLine("Knock Knock");
+            }
+        }
+    }
+}
